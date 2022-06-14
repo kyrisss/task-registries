58db913fb391ea0ab63f4ec22048ce67fa7bee3e
kyrisss <kornev.roma62@gmail.com>
2022-06-14 22:37:13 +0300

    feature: add header & footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import './App.scss';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from 'react'
 import Spinner from './components/Spinner/Spinner';
+import AppHeader from './components/appHeader/AppHeader';
+import AppFooter from './components/appFooter/AppFooter';
+import MainPage from './components/pages/MainPage/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="app">
-                {/* <AppHeader /> */}
+                <AppHeader />
                 <main>
                     <Suspense fallback={<Spinner/>}>
                         <Routes>
-                            {/* <Route path="/" element={<MainPage />} />
-                            <Route path="/login" element={<Login />} />
+                            <Route path="/" element={<MainPage />} />
+                            {/* <Route path="/login" element={<Login />} />
                             <Route path="/personal" element={<Personal />} />
                             <Route path="*" element={<Page404 />} /> */}
                         </Routes>
                     </Suspense>
                 </main>
-                {/* <AppFooter /> */}
+                <AppFooter />
             </div>
     </BrowserRouter>
 
--- src/components/appHeader/AppHeader.tsx
+++ src/components/appHeader/AppHeader.tsx
@@ -1,22 +1,29 @@
 import { Link, NavLink } from 'react-router-dom';
 import './appHeader.scss';
+import sprite from '../../assets/sprite.svg';
+import HeaderProfile from './HeaderProfile';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
-            <h1 className="app__title">
-                <Link to='/'>
-                    <span>Marvel</span> information portal
-                </Link>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    
-                    <li><NavLink to='/' style={({isActive}) => ({"color": isActive? "#9F0013" : "#232222"})}>Characters</NavLink></li>
-                    /
-                    <li><NavLink to='comics' style={({isActive}) => ({"color": isActive? "#9F0013" : "#232222"})}>Comics</NavLink></li>
-                </ul>
-            </nav>
+            <div className="wrapper">
+                <div className="header">
+                    <div className="header__logo logo">
+                        <Link to='/'>
+                            <svg className="logo__img">
+                                <use xlinkHref={`${sprite}#logo`} />
+                            </svg>
+                        </Link>
+                    </div>
+                    <div className="header__search search">
+                        <svg className="search__img">
+                            <use xlinkHref={`${sprite}#search`} />
+                        </svg>
+                        <input type="text" className='search__input' placeholder='Поиск' />
+                    </div>
+                    <HeaderProfile />
+                </div>
+            </div>
         </header>
     )
 }
